faffb70c7178d606c879b9a567e24fac17823006
AssassinYin <yoyooscarsutu@gmail.com>
2024-01-27 15:06:36 +0800

    fetch item function completed

--- GGJ2024Test/Assets/Script/QAScript/DatabaseManager.cs
+++ GGJ2024Test/Assets/Script/QAScript/DatabaseManager.cs
@@ -6,6 +6,7 @@ public class DatabaseManager : MonoBehaviour
 {
     [SerializeField] private QuestionDatabase _questionDatabase;
 
+    #region GENERAL METHODS
     public List<Question> GetQuestionListViaGodName(GodName godName)
     {
         foreach (QuestionDictItem questionDictItem in _questionDatabase.script)
@@ -13,5 +14,26 @@ public class DatabaseManager : MonoBehaviour
                 return questionDictItem.Questions;
         return null;
     }
+    public List<Question> GetRandomQuestionListViaGodName(GodName godName)
+    {
+        foreach (QuestionDictItem questionDictItem in _questionDatabase.script)
+            if (questionDictItem.Key == godName)
+                return Shuffle(questionDictItem.Questions);
+        return null;
+    }
+    #endregion
 
+    #region INNER METHODS
+    private List<T> Shuffle<T>(List<T> list)
+    {
+        for (int i = list.Count - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            T temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+        }
+        return list;
+    }
+    #endregion
 }
