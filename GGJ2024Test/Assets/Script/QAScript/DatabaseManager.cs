35db20fc212eb7b79b81ca3d0ce186cc3bc25e83
AssassinYin <yoyooscarsutu@gmail.com>
2024-01-27 15:39:58 +0800

    print text function completed

--- GGJ2024Test/Assets/Script/QAScript/DatabaseManager.cs
+++ GGJ2024Test/Assets/Script/QAScript/DatabaseManager.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DatabaseManager : MonoBehaviour
 {
     [SerializeField] private QuestionDatabase _questionDatabase;
+    [SerializeField] private TextMeshProUGUI question;
+    [SerializeField] private List<TextMeshProUGUI> answers;
+
+    #region UI UPDATE METHODS
+    public void UpdateText(Question input)
+    {
+        question.text = input.Content;
+        answers[0].text = input.Answers[0].Content;
+        answers[1].text = input.Answers[1].Content;
+        answers[2].text = input.Answers[2].Content;
+    }
+    #endregion
 
     #region GENERAL METHODS
     public List<Question> GetQuestionListViaGodName(GodName godName)
@@ -29,9 +43,7 @@ public class DatabaseManager : MonoBehaviour
         for (int i = list.Count - 1; i > 0; i--)
         {
             int j = Random.Range(0, i + 1);
-            T temp = list[i];
-            list[i] = list[j];
-            list[j] = temp;
+            (list[j], list[i]) = (list[i], list[j]);
         }
         return list;
     }
